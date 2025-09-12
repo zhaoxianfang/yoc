9b5905fe9d74b575212b748f0fc2617a6d1fa85d
zhaoxianfang <1748331509@qq.com>
2025-09-12 17:44:05 +0800

    fix

--- Modules/Home/Resources/views/layouts/top_bar.blade.php
+++ Modules/Home/Resources/views/layouts/top_bar.blade.php
@@ -54,53 +54,21 @@
 
             <!-- Search -->
             <div class="app-search d-xl-flex flex-horizontal-top-search z-9999">
-                <input type="search" class="form-control topbar-search mt-1" name="search" placeholder="搜索...">
+                <input type="search" class="form-control topbar-search" name="search" placeholder="搜索...">
                 <i data-lucide="search" class="app-search-icon text-muted"></i>
             </div>
         </div>
 
         <div class="d-flex align-items-center gap-1">
             <!-- Language Dropdown -->
-            <div class="topbar-item">
-                <div class="dropdown">
-                    <button class="topbar-link fw-bold" data-bs-toggle="dropdown" data-bs-offset="0,21" type="button" aria-haspopup="false" aria-expanded="false">
-                        <img src="{{ asset('static/inspinia/v4.0/assets/images/flags/cn.svg') }}" alt="user-image" class="w-100 rounded me-1" height="18" id="selected-language-image">
-                        <span id="selected-language-code"> 中文 </span>
-                    </button>
-                    <div class="dropdown-menu dropdown-menu-end">
-                        <a href="javascript:void(0);" class="dropdown-item" data-translator-lang="cn" title="中文">
-                            <img src="{{ asset('static/inspinia/v4.0/assets/images/flags/cn.svg') }}" alt="中文" class="me-1 rounded" height="18" data-translator-image>
-                            <span class="align-middle">中文</span>
-                        </a>
-                        <a href="javascript:void(0);" class="dropdown-item" data-translator-lang="en" title="English">
-                            <img src="{{ asset('static/inspinia/v4.0/assets/images/flags/us.svg') }}" alt="English" class="me-1 rounded" height="18" data-translator-image>
-                            <span class="align-middle">English</span>
-                        </a>
-                        <a href="javascript:void(0);" class="dropdown-item" data-translator-lang="de" title="German">
-                            <img src="{{ asset('static/inspinia/v4.0/assets/images/flags/de.svg') }}" alt="German" class="me-1 rounded" height="18" data-translator-image>
-                            <span class="align-middle">Deutsch</span>
-                        </a>
-                        <a href="javascript:void(0);" class="dropdown-item" data-translator-lang="it" title="Italian">
-                            <img src="{{ asset('static/inspinia/v4.0/assets/images/flags/it.svg') }}" alt="Italian" class="me-1 rounded" height="18" data-translator-image>
-                            <span class="align-middle">Italiano</span>
-                        </a>
-                        <a href="javascript:void(0);" class="dropdown-item" data-translator-lang="es" title="Spanish">
-                            <img src="{{ asset('static/inspinia/v4.0/assets/images/flags/es.svg') }}" alt="Spanish" class="me-1 rounded" height="18" data-translator-image>
-                            <span class="align-middle">Español</span>
-                        </a>
-                        <a href="javascript:void(0);" class="dropdown-item" data-translator-lang="ru" title="Russian">
-                            <img src="{{ asset('static/inspinia/v4.0/assets/images/flags/ru.svg') }}" alt="Russian" class="me-1 rounded" height="18" data-translator-image>
-                            <span class="align-middle">Русский</span>
-                        </a>
-                        <a href="javascript:void(0);" class="dropdown-item" data-translator-lang="hi" title="Hindi">
-                            <img src="{{ asset('static/inspinia/v4.0/assets/images/flags/in.svg') }}" alt="Hindi" class="me-1 rounded" height="18" data-translator-image>
-                            <span class="align-middle">हिन्दी</span>
-                        </a>
-                    </div> <!-- end dropdown-menu-->
-                </div> <!-- end dropdown-->
-            </div>
+            @include('users::layouts.top_nav.nav_language')
 
-            @include('home::layouts.notice')
+            <!-- Messages Dropdown -->
+            @include('users::layouts.top_nav.nav_messages')
+            <!-- end topbar item-->
+
+            <!-- Notification Dropdown -->
+            @include('users::layouts.top_nav.nav_notification')
 
             <!-- Light/Dark Mode Button -->
             <div class="topbar-item d-none d-sm-flex">
